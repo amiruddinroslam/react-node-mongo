66b765ecf021160e18113354c9ea5e24e68d67a2
amiruddin <amiruddin@hitachi-ebworx.com>
2020-08-12 12:25:22 +0800

    remove unused function

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -6,11 +6,6 @@ import AddSubscription from './AddSubscription';
 class Subscription extends Component {
 
     state = {
-        // amount: 0,
-        // subscription_type: '',
-        // subscsription_cycle: '',
-        // start_date: '',
-        // end_date: ''
         subscription: []
     }; 
 
@@ -28,19 +23,6 @@ class Subscription extends Component {
                 }
             })
             .catch(err => console.log(err));
-    };
-
-    getList = () => {
-        this.state.subscription.map((sub, index) => {
-            return (
-                <ul className="list-group" key={index}>
-                    <li className="list-group-item">Amount: {sub.amount}</li>
-                    <li className="list-group-item">Subscription type: {sub.subscription_type}</li>
-                    <li className="list-group-item">Subscription cycle: {sub.subscription_cycle}</li>
-                    <li className="list-group-item">Start date: {sub.start_date}</li>
-                    <li className="list-group-item">End date: {sub.end_date}</li>
-                </ul>);
-        });
     };
 
     render() {
